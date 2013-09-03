e9786a4026376256cceb942276e1018becbaa56a
Evanwzy <asee@EvantekiMacBook-Pro.local>
2013-09-03 14:06:57 +0800

    First Commit

---
+++ Cars/Cars/RKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RKAppDelegate.h
+//  Cars
+//
+//  Created by Asee on 9/3/13.
+//  Copyright (c) 2013 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cars/Cars/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cars
+//
+//  Created by Asee on 9/3/13.
+//  Copyright (c) 2013 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RKAppDelegate class]));
+    }
+}
